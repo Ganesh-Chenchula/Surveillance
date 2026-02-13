60d4ba77c4953b4451001fdc39311af9dbd45d94
Ganesh-Chenchula <ganeshchenchula28@gmail.com>
2026-02-13 15:20:09 +0530

    updated settings

--- smart_surveillance/settings.py
+++ smart_surveillance/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-gm64atf#e*0+#_t)9xr+f^n_vb!2@z4u7cgfmxp6y)x&k5u-pq"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['31.97.202.194']
 
 
 # Application definition
@@ -117,7 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+CSRF_TRUSTED_ORIGINS = ["https://31.97.202.194", "http://31.97.202.194"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
